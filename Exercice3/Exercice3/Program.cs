f85e113d0f6341b10b330804e5ea1b384583efef
https://github.com/jorgeposterla <jorgeposterla@hotmail.com>
2022-12-26 21:17:48 +0000

    Exercice 3, class and object creation training encapsulation.

---
+++ Exercice3/Exercice3/Program.cs
@@ -0,0 +1,15 @@
+﻿class Person
+{
+    private int Age { get; set; }
+    private string Name { get; set; }
+    private string PhoneNumber { get; set; }
+
+    private static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Age = 40;
+        person.Name= "Test";
+        person.PhoneNumber= "1234567890";
+        Console.WriteLine($"{person.Name} is {person.Age} years old, and the phone number is {person.PhoneNumber}");
+    }
+}
