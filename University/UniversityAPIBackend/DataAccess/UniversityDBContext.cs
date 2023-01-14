423e9f3a9f11a65a0326ef9f835caa654df7ceab
https://github.com/jorgeposterla <jorgeposterla@hotmail.com>
2023-01-14 23:36:08 +0000

    Exercice 1 (Curso de APIs con .NET)

---
+++ University/UniversityAPIBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityAPIBackend.Models.DataModels;
+
+namespace UniversityAPIBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+
+        //Add DbSets (Tables of our database)
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ University/UniversityAPIBackend/Models/DataModels/Course.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityAPIBackend.Models.DataModels
+{
+    public class Course
+    {
+        [Required, MaxLength(50)] public string Name { get; set; } = string.Empty;
+        [MaxLength(280)] public string ShortDescription { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string TargetAudiences { get; set; } = string.Empty;
+        public string Objectives { get; set; } = string.Empty;
+        public string Requirements { get; set; } = string.Empty;
+        public Level Level { get; set; }
+    }
+
+    public enum Level
+    {
+        Basic,
+        Intermediate,
+        Advanced
+    }
+}
